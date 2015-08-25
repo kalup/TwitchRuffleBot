be691258dd1372a510054bc7d9674c9b58c395cb
kalup <kalup@users.noreply.github.com>
2015-08-25 17:41:33 +0200

    Create twitchRuffleBot.user.js

---
+++ twitchRuffleBot.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        twitch spam bot
+// @namespace   void.com
+// @description SpamBot for twitch, it send desired message
+// @include     http://www.twitch.tv/*
+// @version     1
+// @updateURL   https://raw.githubusercontent.com/kalup/TwitchRuffleBot/master/twitchRuffleBot.user.js
+// @grant       none
+// ==/UserScript==
+
