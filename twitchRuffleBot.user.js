6e44ca5e181b25db006f6ffa3d6f7caa5a084b63
kalup <kalup@users.noreply.github.com>
2015-08-25 18:26:04 +0200

    Update twitchRuffleBot.user.js

--- twitchRuffleBot.user.js
+++ twitchRuffleBot.user.js
@@ -3,11 +3,13 @@
 // @namespace   void.com
 // @description SpamBot for twitch, it send desired message
 // @include     http://www.twitch.tv/*
-// @version     1.0.2
+// @version     1.0.4
 // @updateURL   https://raw.githubusercontent.com/kalup/TwitchRuffleBot/master/twitchRuffleBot.user.js
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
+
+var botRefreshTime = 10000;
 
 window.addEventListener
 (
@@ -17,16 +19,34 @@ window.addEventListener
     var chatContainer =  document.querySelector('.ember-chat');
     
     var wordDiv = document.createElement('div');
+    wordDiv.setAttribute('style','padding:10px');
+    
     var wordTextInput = document.createElement('input');
     wordTextInput.setAttribute('type','text');
     wordTextInput.setAttribute('id','KS_wordTextInput');
+    wordTextInput.setAttribute('style','margin-right:10px; margin-left:50px');
+    
     var wordButton = document.createElement('button');
-    wordButton.addEventListener('click',function(){alert("Mi hai premuto");},false);
+    wordButton.value = "Spamma";
+    wordButton.addEventListener('click',function() {
+      
+      GM_setValue('wordToSpam',document.getElementById('KS_wordTextInput').value);
+      setTimeout(function() {
+        
+        GM_setValue('wordToSpam',"");
+        
+      },botRefreshTime*2);
+      
+    },false);
+    
+    var hr = document.createElement('hr');
     
     wordDiv.appendChild(wordTextInput);
     wordDiv.appendChild(wordButton);
+    wordDiv.appendChild(hr);
     
     chatContainer.insertBefore(wordDiv,chatContainer.firstChild);
+    
   },
   false
 );
